9974f8631c724e402b452bc716b5e735bc6a18b2
Kevin Cruz <kcruz538@gmail.com>
2014-10-25 22:09:11 -0500

    sessions are done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password]
+    # do whatever da fuck
+
+    # do we have a user with that username and password
+
+    user = User.find_by username: username
+    if user.password == password
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def signout
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def signup
+    @user = User.new
+  end
+
+  def create_user
+    @user = User.new(params.require(:user).permit(:username, :password, :password_confirmation))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :signup
+    end
+  end
+end
---
+++ app/controllers/thoughts_controller.rb
@@ -0,0 +1,25 @@
+class ThoughtsController < ApplicationController
+
+  before_action do
+    if session[:user_id].present?
+    else
+      redirect_to sign_in_path
+    end
+  end
+
+  def index
+    @users = User.all
+    @thoughts = current_user.thoughts.order("created_at desc")
+    @thought = Thought.new
+  end
+
+  def post
+    @post = Thought.new(params.require(:thought).permit(:deep_thought))
+    @post.user = current_user
+    if @post.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+end
