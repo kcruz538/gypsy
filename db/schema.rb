8f888100a33063e3dbbc66a3e6e6c4b8ae87370b
Kevin Cruz <kcruz538@gmail.com>
2014-10-27 09:09:45 -0500

    almost done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026000016) do
+ActiveRecord::Schema.define(version: 20141027014653) do
+
+  create_table "followings", force: true do |t|
+    t.integer  "person_id"
+    t.integer  "follower_id"
+    t.boolean  "blocked"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "thoughts", force: true do |t|
     t.integer  "user_id"
